b06e06763a61c9c46dbd6fc4fe9475b45eb17bcf
Shaun Gold <sfgold2@gmail.com>
2022-01-27 22:36:49 -0800

    changed test

--- scraper.py
+++ scraper.py
@@ -126,7 +126,7 @@ def extract_next_links(url, resp):
 
 
         # Check if the page has high textual information content
-        if len(filteredTokens) <= 20:
+        iflen(filteredTokens) <= 20:
             pass
         else:
             # extract all the links in the document
