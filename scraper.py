dd1f051773e2f8b40c24e2b4cd63ca27cbffbc16
Shaun Gold <sfgold2@gmail.com>
2022-01-27 17:59:50 -0800

    moved for loop in extract_next_links

--- scraper.py
+++ scraper.py
@@ -95,16 +95,6 @@ def extract_next_links(url, resp):
         # creates the soup object to extract all the text
         soup = BeautifulSoup(htmlContent,'html.parser')
 
-        # extract all the links in the document
-        for link in soup.find_all('a'):
-            if isSubdomain(url):
-                if url not in subDomains:
-                    subDomains[url] = 1
-                else:
-                    subDomains[url] += 1
-            if is_valid(link):
-                links.append(link.get('href'))
-
 
         # get all text from the document in one string
         fullText = soup.get_text().lower()
@@ -130,6 +120,17 @@ def extract_next_links(url, resp):
         # numOfTokenPerURL used later to find the longest URL by word count
         # within addUniquePage function - Ayako
         numOfTokenPerURL[resp.url] = filteredTokens.len()
+
+
+        # extract all the links in the document
+        for link in soup.find_all('a'):
+            if isSubdomain(url):
+                if url not in subDomains:
+                    subDomains[url] = 1
+                else:
+                    subDomains[url] += 1
+            if is_valid(link):
+                links.append(link.get('href'))
 
 
     else:
